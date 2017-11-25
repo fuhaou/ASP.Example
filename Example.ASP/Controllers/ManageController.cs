88a54fa6d353e031d756d3a653813fcfe49e298d
Haou <chenfuhaou@gmail.com>
2017-11-25 08:53:18 +0700

    BK1 test commmit

--- Example.ASP/Controllers/ManageController.cs
+++ Example.ASP/Controllers/ManageController.cs
@@ -11,6 +11,7 @@ using Example.ASP.Models;
 namespace Example.ASP.Controllers
 {
     [Authorize]
+	//sdfhosiefkasefilhaep sdzdsdffa
     public class ManageController : Controller
     {
         private ApplicationSignInManager _signInManager;
